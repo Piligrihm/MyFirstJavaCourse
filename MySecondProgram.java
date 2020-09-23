7037764196996342e2ec4854aaae69fd8773f928
Alex Shutovich <piligrihm@gmail.com>
2020-09-23 20:06:17 +0300

    remote work

--- MySecondProgram.java
+++ MySecondProgram.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class MySecondProgram{
 
 	public static void main(String [] chtougodno){
-		System.out.println("Vvedite imya");
+		System.out.println("Vvedite imya !!!!!!!!");
 		Scanner scan = new Scanner(System.in);
 		String name = scan.nextLine();
 		
