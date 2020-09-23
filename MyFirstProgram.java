4135feca394994eaf56ad20384643d4f1573deab
Alex Shutovich <piligrihm@gmail.com>
2020-09-23 13:37:22 +0300

    first commit

---
+++ MyFirstProgram.java
@@ -0,0 +1,9 @@
+public class MyFirstProgram{
+	public static void main(String[] args){
+		for (String arg: args){
+			System.out.println("Hi " + arg);
+		
+		}
+		System.out.println("Hello World");
+		}
+}
