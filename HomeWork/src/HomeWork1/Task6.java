5546e6f436a9c0f3f946a424f09c43a3ede4b9c4
Alex Shutovich <piligrihm@gmail.com>
2020-09-27 22:47:15 +0300

    HomeWork1Task6

--- HomeWork/src/HomeWork1/Task6.java
+++ HomeWork/src/HomeWork1/Task6.java
@@ -1,7 +1,27 @@
 package HomeWork1;
 
+import java.util.Arrays;
+
 public class Task6 {
     public static void main(String[] args) {
 
+        createPhoneNumber(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0});
+
+    }
+
+    public static String createPhoneNumber(int[] numbers) {
+
+        // Создаем строку из массива целых чисел. Затем убираем пробелы, запятые и ковычки
+
+        String numberFromArray = Arrays.toString(numbers).replaceAll("\\[|\\]|,| ", "");
+
+        // Добавляем скобки и тире в нужных местах. Фактически сотавляю новую строку из нужных символов и кусочков старой строки
+
+        String result = "(" + numberFromArray.substring(0, 3) + ") " + numberFromArray.substring(3, 6) + "-" + numberFromArray.substring(6, 10);
+
+        System.out.println(result);
+
+
+        return result;
     }
 }
