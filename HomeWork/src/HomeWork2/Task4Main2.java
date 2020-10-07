d15f269e85ea034a8c67071bfed06be62e673892
Alex Shutovich <piligrihm@gmail.com>
2020-10-07 13:19:55 +0300

    Task4Main1 - Bubble sorted and Task4Main2 Shaker sorted

---
+++ HomeWork/src/HomeWork2/Task4Main2.java
@@ -0,0 +1,39 @@
+package HomeWork2;
+
+import java.util.Arrays;
+
+public class Task4Main2 {
+    public static void main(String[] args) {
+        System.out.println("Сортировка перемешиванием (Шейкерная сортировка)");
+
+        int[] arr1 = {1, 2, 3, 4, 5, 6};
+        System.out.println("\nМассив № 1 до сортировки - " + Arrays.toString(arr1));
+        for (int elements: arr1){
+            Sorters.shakerSort(arr1);
+        }
+        System.out.println("Массив № 1 после сортировки - " +Arrays.toString(arr1));
+
+        int[] arr2 = {1,1,1,1};
+        System.out.println("\nМассив № 2 до сортировки - " +Arrays.toString(arr2));
+        for (int elements: arr2){
+            Sorters.shakerSort(arr2);
+        }
+        System.out.println("Массив № 2 после сортировки - " +Arrays.toString(arr2));
+
+
+        int[] arr3 = {9,1,5,99,9,9};
+        System.out.println("\nМассив № 3 до сортировки - " + Arrays.toString(arr3));
+        for (int elements: arr3){
+            Sorters.shakerSort(arr3);
+        }
+        System.out.println("Массив № 3 после сортировки - " +Arrays.toString(arr3));
+
+        int[] arr4 = {};
+        System.out.println("\nМассив № 4 до сортировки - " +Arrays.toString(arr4));
+        for (int elements: arr4){
+            Sorters.shakerSort(arr4);
+        }
+        System.out.println("Массив № 4 после сортировки - " +Arrays.toString(arr4));
+
+    }
+}
